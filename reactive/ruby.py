3b4fcaa366ad7a95a048d325c6b8d74e1ee1f4ba
jamesbeedy <jamesbeedy@users.noreply.github.com>
2016-07-17 16:14:26 -0700

    Add reactive hooks (#4)
    * Add reactive hooks
    
    * Added @when_not('ruby.installed') in place of @hook('install')
    * Added ruby_avail handler, config.changed handler for ruby version
    
    * remove config-changed hook

--- reactive/ruby.py
+++ reactive/ruby.py
@@ -1,7 +1,8 @@
 from charms.reactive import (
-    hook,
     set_state,
-    remove_state
+    remove_state,
+    when,
+    when_not
 )
 
 from charmhelpers.core import hookenv
@@ -13,15 +14,11 @@ import rubylib
 config = hookenv.config()
 
 
-@hook('install')
+@when_not('ruby.installed')
 def install_ruby():
+
     """ Installs defined ruby
-
-    Emits:
-    ruby.available: Emitted once the runtime has been installed
     """
-    remove_state('ruby.available')
-
     # Cleanup any packaged ruby
     hookenv.log('Removing any packaged Ruby', 'debug')
     apt_purge(['ruby'])
@@ -38,5 +35,24 @@ def install_ruby():
     # Install
     rubylib.compile_ruby()
 
+    set_state('ruby.installed')
+
+
+@when('ruby.installed')
+def ruby_avail():
+
+    """ Sets the ruby available state
+
+    Emits:
+    ruby.available: Emitted once the runtime has been installed
+    """
     hookenv.status_set('active', 'Ruby is ready!')
     set_state('ruby.available')
+
+
+@when_not('ruby.installed')
+def ruby_unavail():
+
+    """ Sets remove ruby.available
+    """
+    remove_state('ruby.available')
