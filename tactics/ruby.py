2d58150c67b578bc13517b6a4c5c9f7783190210
Marco Ceppi <marco@ceppi.net>
2016-08-30 14:21:36 -0400

    Modernize layer to use new primitives

---
+++ tactics/ruby.py
@@ -0,0 +1,11 @@
+
+from charmtools.build.tactics import ExactMatch, Tactic
+
+
+class DependenciesTxtTactic(ExactMatch, Tactic):
+    FILENAME = 'dependencies.txt'
+    kind = "dynamic"
+
+    def __call__(self):
+        raise Exception('dependencies.txt is deprecated see '
+                        'https://github.com/battlemidget/juju-layer-ruby/pull/8')
